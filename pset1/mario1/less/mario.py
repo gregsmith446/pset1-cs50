5c36aeac0269453e2205f7f852adccff3a1c14b4
Greg Smith <gregsmith446@gmail.com>
2018-09-04 16:40:00 +0000

    completed mario in python

--- pset1/mario1/less/mario.py
+++ pset1/mario1/less/mario.py
@@ -8,27 +8,22 @@ while True:
     # always prompt user for input first
     height = get_int("Input integer for height: ")
     # if user input IS in range
-    if height > 0 and height < 23:
+    if height >= 0 and height <= 23:
         print("The height is: ", height)
-        print("Here is the pyramid you asked for")
         break
-    else:
+    elif height < 0 and height > 23:
         # reprompt the user
         print("Int must be between 0 and 23")
         height = get_int("Input integer for height: ")
-        print("The height is: ", height)
-        print("Here is the pyramid you asked for")
         break
 
-# use for loop to print out this many rows (aka new lines \n)
-# this will print as many rows as the user gives in int form
+# for each loop iterates through height int
+# ex/ 4 --> 1, 2, 3, 4
 for i in range(height):
-    # print spaces
-    print(" ", end="")
-    # print hashes
-    print("#" * height)
-    # print new line
-    print()
+    # print spaces * (height - i)
+    # print hashtags * (i + 2)
+    print(" " * (height - i) + "#" * (i + 2))
+
 
 
 
